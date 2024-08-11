4629e5beefaca53c2c1f847204acfb5800175e85
Arseniy Gorbikov <gorbikovarseniy@gmail.com>
2024-08-11 13:21:42 +0300

    HW # 38

---
+++ src/main/java/telran/employees/CompanyAppl.java
@@ -0,0 +1,25 @@
+package telran.employees;
+
+import telran.io.Persistable;
+import telran.view.Item;
+import telran.view.Menu;
+import telran.view.SystemInputOutput;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class CompanyAppl {
+    private static final String FILE_NAME = "employeesTest.data";
+
+    public static void main(String[] args) {
+        Company company = new CompanyMapsImpl();
+        try {
+            ((Persistable)company).restore(FILE_NAME);
+        } catch (Exception e) {}
+        List<Item> companyItems = CompanyApplItems.getCompanyItems(
+                company, new HashSet<String>(List.of("Audit", "Development", "QA")));
+        companyItems.add(Item.of("Exit & save", inputOutput -> ((Persistable)company).save(FILE_NAME), true));
+        Menu menu = new Menu("Company CLI Application", companyItems.toArray(Item[]::new));
+        menu.perform(new SystemInputOutput());
+    }
+}
---
+++ src/main/java/telran/employees/CompanyApplItems.java
@@ -0,0 +1,119 @@
+package telran.employees;
+
+import telran.view.InputOutput;
+import telran.view.Item;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CompanyApplItems {
+    static Company company;
+    static HashSet<String> departments;
+
+    public static List<Item> getCompanyItems(Company company, HashSet<String> departments) {
+        CompanyApplItems.company = company;
+        CompanyApplItems.departments = departments;
+        Item[] items = {
+                Item.of("add employee", (InputOutput inputOutput) -> {
+                    addEmployee(inputOutput);
+                }),
+                Item.of("display employee data", CompanyApplItems::getEmployee),
+                Item.of("remove employee", CompanyApplItems::removeEmployee),
+                Item.of("display department budget", CompanyApplItems::getDepartmentBudget),
+                Item.of("display departments", CompanyApplItems::getDepartments),
+                Item.of("display managers with most factor", CompanyApplItems::getManagersWithMostFactor),
+        };
+        return new ArrayList<>(List.of(items));
+    }
+
+    private static void getManagersWithMostFactor(InputOutput inputOutput) {
+        Manager[] managers = company.getManagersWithMostFactor();
+        String line = managers.length == 0 ? "no managers" :
+                Arrays.stream(managers).map(Employee::getJSON)
+                        .collect(Collectors.joining("\n"));
+        inputOutput.writeLine(line);
+    }
+
+    private static void getDepartments(InputOutput inputOutput) {
+        String departments = company.getDepartments().length == 0 ?
+                "No department has been created yet" :
+                Arrays.stream(company.getDepartments()).map(dep -> dep + " ").collect(Collectors.joining());
+        inputOutput.writeLine(departments);
+    }
+
+    private static void getDepartmentBudget(InputOutput inputOutput) {
+        String department = inputOutput.readStringOptions("Enter department ", "Wrong department or no employees woring in entered department", new HashSet<>(List.of(company.getDepartments())));
+        inputOutput.writeLine("The budget of " + department + " department : " + company.getDepartmentBudget(department));
+    }
+
+    private static void removeEmployee(InputOutput inputOutput) {
+        long id = getId(inputOutput);
+        company.removeEmployee(id);
+        inputOutput.writeLine("Employee was successful removed");
+    }
+
+    private static void getEmployee(InputOutput inputOutput) {
+        long id = getId(inputOutput);
+        Employee empl = company.getEmployee(id);
+        String line = empl == null ? "no employee with the entered ID"
+                : empl.getJSON();
+        inputOutput.writeLine(line);
+    }
+
+    private static void addEmployee(InputOutput inputOutput) {
+        Employee employee = readEmployee(inputOutput);
+        String type = inputOutput.readStringOptions("Enter employee type", "Wrong Employee type",
+                new HashSet<String>(List.of(
+                        "WageEmployee",
+                        "Manager",
+                        "SalesPerson")));
+        Employee result = switch (type) {
+            case "WageEmployee" -> getWageEmployee(employee, inputOutput);
+            case "Manager" -> getManager(employee, inputOutput);
+            case "SalesPerson" -> getSalesPerson(employee, inputOutput);
+            default -> null;
+        };
+        company.addEmployee(result);
+        inputOutput.writeLine("Employee has been added");
+    }
+
+    private static Employee getSalesPerson(Employee employee, InputOutput inputOutput) {
+        WageEmployee wageEmployee = (WageEmployee) getWageEmployee(employee, inputOutput);
+        float percents = inputOutput.readNumberRange("Enter percents", "Wrong percents value", 0.5, 2).floatValue();
+        long sales = inputOutput.readNumberRange("Enter Sales", "Wrong sales valut", 500, 50000).longValue();
+        return new SalesPerson(
+                employee.getId(),
+                employee.getBasicSalary(),
+                employee.getDepartment(),
+                wageEmployee.getHours(),
+                wageEmployee.getWage(),
+                percents,
+                sales);
+    }
+
+    private static Employee getManager(Employee employee, InputOutput inputOutput) {
+        float factor = inputOutput.readNumberRange("Enter factor", "Wrong factor", 1.5, 5).floatValue();
+        return new Manager(employee.getId(), employee.getBasicSalary(), employee.getDepartment(), factor);
+    }
+
+    private static Employee getWageEmployee(Employee employee, InputOutput inputOutput) {
+        int hours = inputOutput.readNumberRange("Enter working hours", "Wrong working hours value", 10, 200).intValue();
+        int wage = inputOutput.readNumberRange("Enter hour wage", "Wrong wage value", 100, 1000).intValue();
+        return new WageEmployee(employee.getId(), employee.getBasicSalary(), employee.getDepartment(), hours, wage);
+    }
+
+    private static Employee readEmployee(InputOutput inputOutput) {
+        long id = getId(inputOutput);
+        int basicSalary = inputOutput.readNumberRange("Enter basic salary", "Wrong basic salary value", 2000, 20000).intValue();;
+        String department = inputOutput.readStringOptions("EnterDepartment " + departments, "Wrong department", departments);
+        return new Employee(id, basicSalary, department);
+    }
+
+    private static long getId(InputOutput inputOutput) {
+        long id = inputOutput.readNumberRange("Enter id value", "Wrong id value",1000, 10000).longValue();
+        return id;
+    }
+}
